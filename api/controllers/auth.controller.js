6b8a5eaf33195e95205895135bf3f40cbfce6ada
yogitaa-chavan <yogitachavan0117@gmail.com>
2024-07-03 11:08:51 +0530

    create signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/User.model.js'
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../users/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
 
@@ -13,6 +14,24 @@ export const signup = async (req, res, next) => {
     res.status(201).json('User created successfully!');
 
     } catch (error){
+        next(error);
+    }
+};
+
+export const signin = async (req, res, next) =>{
+    const {email, password}= req.body;
+    try {
+
+        const validUser =await User.findOne({email});
+        if(!validUser) return next(errorHandler(404, 'User not found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401,'Invalid password'));
+        const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET)
+        const {password: pass, ...rest} = validUser._doc;
+        res.cookie('access_token', token, { httpOnly: true})
+        .status(200)
+        .json(rest)
+    }catch (error){
         next(error);
     }
 };
