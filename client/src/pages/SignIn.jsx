68c6e7a106f8c3e77229a6493839017aa24c8881
yogitaa-chavan <yogitachavan0117@gmail.com>
2024-07-03 12:11:10 +0530

    redux toolkit added

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import {Link, useNavigate} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import {signInStart , signInSuccess , signInFailure} from '../redux/user/userSlice'
 
 
 export default function SignIn() {
 
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const[loading, setloading] = useState(false);
+  const {loading, error} = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange =(e)=>{
     setFormData({
       ...formData,
@@ -18,7 +20,8 @@ export default function SignIn() {
   const handleSubmit = async(e)=>{
     e.preventDefault();
     try{
-      setloading(true);
+
+      dispatch(signInStart());
     const res = await fetch('/api/auth/signin', 
       {
         method:'POST',
@@ -31,16 +34,13 @@ export default function SignIn() {
     const data=await res.json();
     console.log(data);
     if(data.success===false){
-      setError(data.message);
-      setloading(false);
+      dispatch(signInFailure(data.message));
       return;
     }
-    setloading(false);
-    setError(null);
+    dispatch(signInSuccess(data));
     navigate('/');
     }catch(error){
-      setloading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));
     }
     
     
---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,31 @@
+import { createSlice, current } from '@reduxjs/toolkit';
+
+const initialState ={
+    currentUser: null,
+    error:null,
+    loading: false,
+};
+
+const UserSlice = createSlice({
+    name:'user',
+    initialState,
+    reducers:{
+        signInStart: (state) =>{
+            state.loading = true;
+        },
+        signInSuccess: (state, action) =>{
+            state.currentUser = action.payload;
+            state.loading=false;
+            state.error=null;
+        },
+        signInFailure: (state, action)=> {
+            state.error = action.payload;
+            state.loading=false;
+        }
+    }
+});
+
+export const {signInStart , signInSuccess , signInFailure}= UserSlice.actions;
+
+
+export default UserSlice.reducer;
