e8d790ed7d8f3b8ba4884fb2bcc988223c1bd300
yogitaa-chavan <yogitachavan0117@gmail.com>
2024-07-04 20:08:58 +0530

    update the header and make the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './Components/Header';
+import PrivateRoute from './Components/PrivateRoute';
 
 
 export default function App() {
@@ -16,8 +17,9 @@ export default function App() {
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/about" element={<About />} />
+      <Route element={<PrivateRoute/>}>
       <Route path="/profile" element={<Profile />} />
-
+      </Route>
     </Routes>
       
     </BrowserRouter>;
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,8 +1,10 @@
 import {FaSearch} from 'react-icons/fa';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
+  const {currentUser} = useSelector(state=> state.user)
   return (
     <header className='bg-slate-200 shadow-nd'>
         <div className="flex justify-between items-center max-w-6xl mx-auto">
@@ -19,11 +21,18 @@ export default function Header() {
         <ul className='flex gap-4'>
           <Link to=''>
           <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li> </Link>
+
           <Link to='/about'>
           <li className='hidden sm:inline text-slate-700 hover:underline'>About</li></Link>
-          <Link to='/sign-in'>
-          <li className='text-slate-700 hover:underline'>Sign In</li>
-          </Link>
+          
+          <Link to='/profile'>
+          {currentUser? (
+            <img className='rounded-ful h-7 w-7 object-cover ' src="currentUser.avator" alt="profile" />
+          ):(<li className='text-slate-700 hover:underline'>Sign In</li>
+       
+        )}
+        </Link>
+          
         </ul>
         </div>
     </header>
