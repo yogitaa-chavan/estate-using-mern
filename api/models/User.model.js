27056e3397f5fe002b4e74a483096f68d02daac9
yogitaa-chavan <yogitachavan0117@gmail.com>
2024-07-05 20:58:10 +0530

    complete profile page ui

--- api/models/User.model.js
+++ api/models/User.model.js
@@ -18,7 +18,7 @@ password:{
 
 },
 avatar:{
-    type:String,
+    type: String,
     default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
 },
 }, { timestamps: true});
