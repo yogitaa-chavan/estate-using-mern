0b557ad9ab9a25b529d5ce7008e2778fb02221ef
yogitaa-chavan <yogitachavan0117@gmail.com>
2024-07-27 23:28:41 +0530

    complete update listing functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,22 +8,26 @@ import Profile from './pages/Profile';
 import Header from './Components/Header';
 import PrivateRoute from './Components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
+import UpdateListing from './pages/UpdateListing';
 
 
-export default function App() {
-  return <BrowserRouter>
-  <Header/>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/about" element={<About />} />
-      <Route element={<PrivateRoute/>}>
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/create-listing" element={<CreateListing />}/>
+export default function App() 
+{
+  return (
+    <BrowserRouter>
+       <Header/>
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/about" element={<About />} />
+            <Route element={<PrivateRoute/>}>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/create-listing" element={<CreateListing />}/>
+            <Route path="/update-listing/:listingId" element={<UpdateListing />}/>
       </Route>
     </Routes>
       
-    </BrowserRouter>;
-  
+    </BrowserRouter>
+  );
 }
