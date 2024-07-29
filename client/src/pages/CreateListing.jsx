35a3d4b1256f24a8e6d51f524d1bc3f1400e55d0
yogitaa-chavan <yogitachavan0117@gmail.com>
2024-07-29 22:51:08 +0530

    complete whole listing page functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -230,8 +230,10 @@ export default function CreateListing() {
               />
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
-                <span className='text-xs'>($ / month)</span>
                 
+                {formData.type === 'rent' && (
+                  <span className='text-xs'>($ / month)</span>
+                )}
               </div>
             </div>
             {formData.offer &&(
@@ -248,7 +250,10 @@ export default function CreateListing() {
                />
                <div className='flex flex-col items-center'>
                  <p>Discounted price</p>
-                 <span className='text-xs'>($ / month)</span>
+                 
+                 {formData.type === 'rent' && (
+                    <span className='text-xs'>($ / month)</span>
+                  )}
                </div>
              </div>
             )}
--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -248,8 +248,9 @@ export default function UpdateListing() {
               />
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
-                <span className='text-xs'>($ / month)</span>
-                
+                {formData.type === 'rent' && (
+                  <span className='text-xs'>($ / month)</span>
+                )}
               </div>
             </div>
             {formData.offer &&(
@@ -266,7 +267,9 @@ export default function UpdateListing() {
                />
                <div className='flex flex-col items-center'>
                  <p>Discounted price</p>
-                 <span className='text-xs'>($ / month)</span>
+                 {formData.type === 'rent' && (
+                    <span className='text-xs'>($ / month)</span>
+                  )}
                </div>
              </div>
             )}
