6843f25c96e3057934f8c3cc9c36a7b02a5e8b73
yogitaa-chavan <yogitachavan0117@gmail.com>
2024-07-29 23:39:30 +0530

    contact update

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
+import {useSelector} from 'react-redux';
+
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
 import {
@@ -13,6 +15,8 @@ import {
   FaParking,
   FaShare,
 } from 'react-icons/fa';
+import Contact from '../Components/Contact';
+
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
@@ -20,7 +24,10 @@ export default function Listing() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [copied, setCopied] = useState(false);
+  const [contact, setContact] =useState(false);
   const params = useParams();
+  const {currentUser} = useSelector((state) => state.user);
+  console.log(currentUser._id, listing?.userRef);
   useEffect(() => {
     const fetchListing = async () => {
       try {
@@ -42,7 +49,7 @@ export default function Listing() {
     };
     fetchListing();
   }, [params.listingId]);
-  console.log(loading);
+  
 
   return (
     <main>
@@ -130,6 +137,11 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button onClick={()=>setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95
+              p-3'>CONTACT</button>
+            )}
+            {contact && <Contact listing={listing}/>}
           </div>
         </div>
       )}
